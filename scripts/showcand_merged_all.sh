aa394ce540635ce34837abd7737141f4775c863b
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2021-12-02 21:19:15 +0800

    added

---
+++ scripts/showcand_merged_all.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+dir_template="*_ch1??_02"
+if [[ -n "$1" && "$1" != "-" ]]; then
+   dir_template=$1
+fi
+
+min_snr=10
+if [[ -n "$2" && "$2" != "-" ]]; then
+   min_snr=$2
+fi
+
+min_dm=0
+if [[ -n "$3" && "$3" != "-" ]]; then
+   min_dm=$3
+fi
+
+
+for dir in `ls -d ${dir_template=}`
+do
+   cd ${dir}
+   
+   for filfile in `ls *_norm.fil`
+   do
+      echo "showcand_merged.sh $filfile $min_snr $min_dm"
+      showcand_merged.sh $filfile $min_snr $min_dm
+   done
+   cd ..
+done
