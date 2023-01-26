dc31cca066a3d61760d8f9b20b8f48577f4ab815
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2023-01-26 16:22:50 +0800

    added

---
+++ scripts/frb_search_simple.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+fits=1358703336_20230125173518_ch120_02_out_t.fits
+if [[ -n "$1" && "$1" != "-" ]]; then
+   fits=$1
+fi
+
+dts=${fits%%.fits}_dts.fits
+dts_minus_one=${fits%%.fits}_dts_minus_one.fits
+dts_snr=${fits%%.fits}_dts_minus_one_div_rmsiqr.fits
+
+echo "dynaspec_search ${fits} ${dts} -l 10 -m 500 -s 1 -t 29600 -n 10 -o 1358703336 -a 0 -d 20 -A -w 0 -S 0 > dynaspec_search.out"
+dynaspec_search ${fits} ${dts} -l 10 -m 500 -s 1 -t 29600 -n 10 -o 1358703336 -a 0 -d 20 -A -w 0 -S 0 > dynaspec_search.out
+
+x_size=`fitshdr ${dts} | grep NAXIS1 | awk '{print $3;}'`
+y_size=`fitshdr ${dts} | grep NAXIS2 | awk '{print $3;}'`
+
+echo "create_fits ${x_size} ${y_size}"
+create_fits ${x_size} ${y_size}
+
+echo "calcfits_bg ${dts} - one.fits ${dts_minus_one}"
+calcfits_bg ${dts} - one.fits ${dts_minus_one}
+
+echo "noise_mapper ${dts_minus_one} -r 5 -i"
+noise_mapper ${dts_minus_one} -r 5 -i
+
+
+echo "calcfits_bg ${dts_minus_one} / noise_map_rmsiqr.fits ${dts_snr}"
+calcfits_bg ${dts_minus_one} / noise_map_rmsiqr.fits ${dts_snr}
+
+# find sources in DTS :
+echo "find_sources_fits ${dts_snr}"
+find_sources_fits ${dts_snr}
+
