02a1c06c3316b591295dfb401913194629f7bbac
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2024-05-19 08:21:04 +0800

    small corrections/improvements

--- scripts/fredda.sh
+++ scripts/fredda.sh
@@ -7,5 +7,8 @@ fi
 
 # cudafdmt dynaspec_00092_00090.fil  -t 512 -d 2048 -S 0 -r 1 -s 1 -m 1 -x 5 -o dynaspec_00092_00090_test.cand
 
-echo "/usr/local/bin//cudafdmt ${filfile} -t 512 -d 2048 -S 0 -r 1 -s 1 -m 100 -x 10 -o merged_channels_1714558263.211720.cand"
-/usr/local/bin//cudafdmt ${filfile} -t 512 -d 2048 -S 0 -r 1 -s 1 -m 100 -x 10 -o merged_channels_1714558263.211720.cand
+candfile=${filfile%%fil}cand
+echo "/usr/local/bin//cudafdmt ${filfile} -t 512 -d 2048 -S 0 -r 1 -s 1 -m 100 -x 10 -o ${candfile}"
+/usr/local/bin//cudafdmt ${filfile} -t 512 -d 2048 -S 0 -r 1 -s 1 -m 100 -x 10 -o ${candfile}
+
+
--- scripts/fredda_all.sh
+++ scripts/fredda_all.sh
@@ -6,10 +6,12 @@ do
    if [[ -s dynspec_avg7_i.fil ]]; then
       echo "~/github/mwafrb/scripts/fredda.sh dynspec_avg7_i.fil"
       ~/github/mwafrb/scripts/fredda.sh dynspec_avg7_i.fil
-      
+
+      pwd      
       echo "wc -l dynspec_avg7_i.cand"
       wc -l dynspec_avg7_i.cand
-      ls -al *.cand
+      ls -al *.cand      
+      echo "python ~/github/mwafrb/scripts/my_friends_of_friends.py dynspec_avg7_i.cand --outfile=dynspec_avg7_i.merged_cand"
       
       sleep 5
    else
