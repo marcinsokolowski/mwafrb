a3a168313ee3763b634d5604e5a659f9f5bb33f4
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2024-05-19 08:16:14 +0800

    corrections

--- scripts/fredda_all.sh
+++ scripts/fredda_all.sh
@@ -2,14 +2,14 @@
 
 for chdir in `ls -d ch???`
 do
-   cd ${chdir}
-   
+   cd ${chdir}   
    if [[ -s dynspec_avg7_i.fil ]]; then
-      echo "~/github/mwafrb/scripts/fredda! dynspec_avg7_i.fil"
-      ~/github/mwafrb/scripts/fredda! dynspec_avg7_i.fil
+      echo "~/github/mwafrb/scripts/fredda.sh dynspec_avg7_i.fil"
+      ~/github/mwafrb/scripts/fredda.sh dynspec_avg7_i.fil
       
       echo "wc -l dynspec_avg7_i.cand"
       wc -l dynspec_avg7_i.cand
+      ls -al *.cand
       
       sleep 5
    else
