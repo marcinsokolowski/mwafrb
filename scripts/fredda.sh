6b9b4fd26e69ca8bf52697a7917f089005f074ff
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2024-05-19 08:07:44 +0800

    example FREDDA execution added

---
+++ scripts/fredda.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+filfile=merged_channels_1714558263.211720.fil
+if [[ -n "$1" && "$1" != "-" ]]; then
+   filfile="$1"
+fi
+
+# cudafdmt dynaspec_00092_00090.fil  -t 512 -d 2048 -S 0 -r 1 -s 1 -m 1 -x 5 -o dynaspec_00092_00090_test.cand
+
+echo "/usr/local/bin//cudafdmt ${filfile} -t 512 -d 2048 -S 0 -r 1 -s 1 -m 100 -x 10 -o merged_channels_1714558263.211720.cand"
+/usr/local/bin//cudafdmt ${filfile} -t 512 -d 2048 -S 0 -r 1 -s 1 -m 100 -x 10 -o merged_channels_1714558263.211720.cand
