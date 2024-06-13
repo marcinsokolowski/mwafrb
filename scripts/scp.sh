34102d30a38b89d1aaa4dc2aae82a13360c268c9
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2024-06-13 11:15:13 +0800

    added script for getting results from the server

---
+++ scripts/scp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+path=/data/2024_04_22_pulsars/J0835-4510_flagants_90ch_ch230/230/filterbank_msok_64ch/
+
+rsync -avP aavs2:${path}/*.cand* .
+rsync -avP aavs2:${path}/total_power.txt .
+rsync -avP aavs2:${path}/fredda_totalpower_4sec.out .
+
+~/github/mwafrb/scripts/plot_median_of_median_check.sh fredda_totalpower_4sec.out
+
+candmerged_file=`ls *.cand_merged | tail -1`
+/home/msok/github/mwafrb/scripts/overplot_candidates_and_totalpower.sh ${candmerged_file}
+
+cand_file=`ls *.cand | tail -1`
+/home/msok/github/mwafrb/scripts/overplot_candidates_and_totalpower.sh ${cand_file}
